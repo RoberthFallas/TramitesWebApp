4c786ceebf35ade73943101a8581a23cf7a9adc1
Roberth <roberth123pz@gmail.com>
2020-09-05 19:57:07 -0600

    Totalidad de proyecto(Puede que no les funcione)

---
+++ src/LogIn.tsx
@@ -0,0 +1,24 @@
+import { app, Component } from 'apprun';
+import Usuario from './Dto/Usuario';
+
+export default class LogInComponent extends Component {
+  state = new Usuario();
+
+  view = state => <div>
+    <input type="text" placeholder="Usuario" oninput={e => this.run('usuario', e)} />
+    <input type="password" placeholder="Contrseña" oninput={e => this.run('contrasenna', e)} />
+    <button>Entrar</button>
+  </div>;
+
+  update = {
+    "contrasenna": (state ,valor) => {
+      console.log(state)
+      this.state.setContrasenna(valor.target.value)
+    },
+    "usuario": (state ,valor) => {
+      this.state.setCedula(valor.target.value)
+      console.log(state)
+    }
+  }
+}
+
